91c470a72af42b7ff65d0f4a7374955632cf88b5
am-gooru <ashish@gooru.org>
2018-10-11 12:04:36 +0530

    Provide first implementation for LP fetcher module. This module fetches LP for specific user and subject which is then used for creating a merged LP baseline for the user.

--- src/main/java/org/gooru/profilebaseline/infra/services/learnerprofile/LearnerProfileProviderImpl.java
+++ src/main/java/org/gooru/profilebaseline/infra/services/learnerprofile/LearnerProfileProviderImpl.java
@@ -1,7 +1,11 @@
 package org.gooru.profilebaseline.infra.services.learnerprofile;
 
+import java.util.List;
 import org.gooru.profilebaseline.infra.data.ProfileBaselineProcessingContext;
+import org.gooru.profilebaseline.infra.services.algebra.competency.Competency;
+import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyAlgebraDefaultBuilder;
 import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyLine;
+import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyMap;
 import org.skife.jdbi.v2.DBI;
 
 /**
@@ -12,6 +16,7 @@ class LearnerProfileProviderImpl implements LearnerProfileProvider {
 
   private final DBI dbi4core;
   private final DBI dbi4ds;
+  private LearnerProfileProviderDao dsDao;
 
   LearnerProfileProviderImpl(DBI dbi4core, DBI dbi4ds) {
     this.dbi4core = dbi4core;
@@ -20,8 +25,20 @@ class LearnerProfileProviderImpl implements LearnerProfileProvider {
 
   @Override
   public CompetencyLine findLearnerProfileForUser(ProfileBaselineProcessingContext context) {
-    // TODO: Implement this
-
-    return null;
+    List<Competency> competenciesForLP = fetchDsDao()
+        .fetchProficiencyForUserInSpecifiedSubjectAndDomains(context.getUserId().toString(),
+            context.getSubject());
+    if (competenciesForLP != null && !competenciesForLP.isEmpty()) {
+      return CompetencyMap.build(competenciesForLP).getCeilingLine();
+    }
+    return CompetencyAlgebraDefaultBuilder.getEmptyCompetencyLine();
   }
+
+  private LearnerProfileProviderDao fetchDsDao() {
+    if (dsDao == null) {
+      dsDao = dbi4core.onDemand(LearnerProfileProviderDao.class);
+    }
+    return dsDao;
+  }
+
 }
