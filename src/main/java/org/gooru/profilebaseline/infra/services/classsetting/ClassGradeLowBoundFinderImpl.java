1d4bd84953c73cbc1a8c9d51385d9925447f86ae
am-gooru <ashish@gooru.org>
2018-10-11 11:23:28 +0530

    Provide wrapper implementation for class low bound finder. This module is supposed to validate the grade lower bound for class, and if valid fetch the competency line for that lower bound

--- src/main/java/org/gooru/profilebaseline/infra/services/classsetting/ClassGradeLowBoundFinderImpl.java
+++ src/main/java/org/gooru/profilebaseline/infra/services/classsetting/ClassGradeLowBoundFinderImpl.java
@@ -1,8 +1,14 @@
 package org.gooru.profilebaseline.infra.services.classsetting;
 
+import java.util.List;
 import org.gooru.profilebaseline.infra.data.ProfileBaselineProcessingContext;
+import org.gooru.profilebaseline.infra.services.algebra.competency.Competency;
+import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyAlgebraDefaultBuilder;
 import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyLine;
+import org.gooru.profilebaseline.infra.services.algebra.competency.CompetencyMap;
 import org.skife.jdbi.v2.DBI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author ashish.
@@ -12,6 +18,10 @@ class ClassGradeLowBoundFinderImpl implements ClassGradeLowBoundFinder {
 
   private final DBI dbi4core;
   private final DBI dbi4ds;
+  private Long gradeLowerBoundInClass;
+  private ClassGradeLowBoundFinderDao dao4core;
+  private ProfileBaselineProcessingContext context;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ClassGradeLowBoundFinderImpl.class);
 
   ClassGradeLowBoundFinderImpl(DBI dbi4core, DBI dbi4ds) {
     this.dbi4core = dbi4core;
@@ -20,8 +30,49 @@ class ClassGradeLowBoundFinderImpl implements ClassGradeLowBoundFinder {
 
   @Override
   public CompetencyLine findLowGradeForClass(ProfileBaselineProcessingContext context) {
-    // TODO: Implement this
+    this.context = context;
+    if (context.isILExperience()) {
+      return CompetencyAlgebraDefaultBuilder.getEmptyCompetencyLine();
+    }
 
-    return null;
+    initializeGradeLowerBoundForClass();
+    if (gradeLowerBoundInClass == null) {
+      LOGGER.info("Grade lower bound not found for class: '{}'. Return with empty low grade line ",
+          context.getClassId());
+      return CompetencyAlgebraDefaultBuilder.getEmptyCompetencyLine();
+    }
+    validateGradeWithMaster();
+
+    return earthlineForGradeAndSubject();
+  }
+
+  private CompetencyLine earthlineForGradeAndSubject() {
+    List<Competency> earthLineCompetencies = fetchDao4Core()
+        .fetchCompetenciesForEarthlineForGradeAndSubject(context.getSubject(),
+            gradeLowerBoundInClass);
+    if (earthLineCompetencies != null && !earthLineCompetencies.isEmpty()) {
+      return CompetencyMap.build(earthLineCompetencies).getCeilingLine();
+    }
+    return CompetencyAlgebraDefaultBuilder.getEmptyCompetencyLine();
+  }
+
+  private void validateGradeWithMaster() {
+    if (!fetchDao4Core().validateGrade(gradeLowerBoundInClass)) {
+      throw new IllegalArgumentException(
+          "Invalid grade specified in class lower bound. Class: " + context.getClassId()
+              + "  Grade: " + gradeLowerBoundInClass);
+    }
+  }
+
+  private void initializeGradeLowerBoundForClass() {
+    gradeLowerBoundInClass = fetchDao4Core()
+        .fetchGradeLowerBoundForSpecifiedClass(context.getClassId());
+  }
+
+  private ClassGradeLowBoundFinderDao fetchDao4Core() {
+    if (dao4core == null) {
+      dao4core = dbi4core.onDemand(ClassGradeLowBoundFinderDao.class);
+    }
+    return dao4core;
   }
 }
