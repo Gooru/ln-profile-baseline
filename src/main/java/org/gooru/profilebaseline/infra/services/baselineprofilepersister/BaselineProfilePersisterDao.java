2fd1efcd1ea33bfd9229f6bfdd2e8289b99f30b8
am-gooru <ashish@gooru.org>
2018-10-31 15:09:11 +0530

    Return the long id correctly

--- src/main/java/org/gooru/profilebaseline/infra/services/baselineprofilepersister/BaselineProfilePersisterDao.java
+++ src/main/java/org/gooru/profilebaseline/infra/services/baselineprofilepersister/BaselineProfilePersisterDao.java
@@ -4,6 +4,7 @@ import org.gooru.profilebaseline.infra.services.baselineprofilepersister.Learner
 import org.skife.jdbi.v2.sqlobject.BindBean;
 import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 /**
  * @author ashish.
@@ -12,7 +13,7 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 interface BaselineProfilePersisterDao {
 
   @GetGeneratedKeys
-  @SqlQuery("insert into learner_profile_baselined (tx_subject_code, class_id, course_id, user_id, gut_codes, lp_data) values (:subjectCode, :classId, :courseId, :userId, :gutCodes, :lpData)")
+  @SqlUpdate("insert into learner_profile_baselined (tx_subject_code, class_id, course_id, user_id, gut_codes, lp_data) values (:subjectCode, :classId, :courseId, :userId, :gutCodes, :lpData)")
   Long persistLearnerProfileBaselined(@BindBean LearnerProfileBaselinedModelBean model);
 
 }
